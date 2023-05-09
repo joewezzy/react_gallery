9cc9c0154e4cc91d36a79d5e85e1fc4356eb1bb0
joe sunesis <joemarbell@gmail.com>
2023-05-09 11:09:14 +0000

    Create form, circular progress, and setting up firebase to file storage

--- src/components/upload/Form.js
+++ src/components/upload/Form.js
@@ -2,21 +2,27 @@ import { AddAPhoto } from "@mui/icons-material";
 import { Fab, Input } from "@mui/material";
 import React from "react";
 
-const Form = ({setFiles}) => {
+const Form = ({ setFiles }) => {
   const fileRef = React.useRef();
 
   const handleClick = () => {
     fileRef.current.click();
-  }
+  };
 
   const handleChange = (e) => {
-    setFiles([...e.target.files])
+    setFiles([...e.target.files]);
     fileRef.current.value = null;
-  }
+  };
 
   return (
     <form>
-      <Input type="File" inputProps={{ multiple:true }} sx={{ display: "none" }} inputRef={fileRef} onChange={handleChange} />
+      <Input
+        type="File"
+        inputProps={{ multiple: true }}
+        sx={{ display: "none" }}
+        inputRef={fileRef}
+        onChange={handleChange}
+      />
       <Fab color="primary" arial-label="add Image" onClick={handleClick}>
         <AddAPhoto fontSize="medium" />
       </Fab>
