33243c9674381e6097be49c0f028501a004a5788
joewezzy <joemarbell@gmail.com>
2023-05-16 10:53:45 +0000

    User Authentication using firebase auth (#4)

---
+++ src/context/AuthContext.js
@@ -0,0 +1,61 @@
+import { useContext, createContext, useState, useEffect } from "react";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { auth } from "../firebase/config";
+
+const authContext = createContext();
+
+export const useAuth = () => {
+  return useContext(authContext);
+};
+
+const AuthContext = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [model, setModel] = useState({isOpen: false, title: '', content: ''});
+  const [alert, setAlert] = useState({isAlert: false, severity: 'info', message: '', timeout: null, location: ''});
+  const [loading, setLoading] = useState(false);
+
+
+  const signUp = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  const loginWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider);
+  }
+
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  const logout = () => {
+    return signOut(auth);
+  }
+  
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setCurrentUser(user);
+      console.log('User status changed: ', user);
+    })
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    signUp,
+    loginWithGoogle,
+    login,
+    logout,
+    model,
+    setModel,
+    alert,
+    setAlert,
+    loading,
+    setLoading
+  };
+
+  return <authContext.Provider {...{ value }}>{children}</authContext.Provider>;
+};
+
+export default AuthContext;
