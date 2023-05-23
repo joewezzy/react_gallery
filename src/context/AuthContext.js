f0a98445325cf5a4a89a39469a8e7dde58abf3fa
joe sunesis <joemarbell@gmail.com>
2023-05-23 15:09:15 +0000

    Reset User Password

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,13 @@
 import { useContext, createContext, useState, useEffect } from "react";
-import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import { auth } from "../firebase/config";
 
 const authContext = createContext();
@@ -10,33 +18,42 @@ export const useAuth = () => {
 
 const AuthContext = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
-  const [model, setModel] = useState({isOpen: false, title: '', content: ''});
-  const [alert, setAlert] = useState({isAlert: false, severity: 'info', message: '', timeout: null, location: ''});
+  const [model, setModel] = useState({ isOpen: false, title: "", content: "" });
+  const [alert, setAlert] = useState({
+    isAlert: false,
+    severity: "info",
+    message: "",
+    timeout: null,
+    location: "",
+  });
   const [loading, setLoading] = useState(false);
-
 
   const signUp = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
-  }
+  };
 
   const loginWithGoogle = () => {
     const provider = new GoogleAuthProvider();
     return signInWithPopup(auth, provider);
-  }
+  };
 
   const login = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
-  }
+  };
 
   const logout = () => {
     return signOut(auth);
-  }
-  
+  };
+
+  const resetPassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, user => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       setCurrentUser(user);
-      console.log('User status changed: ', user);
-    })
+      console.log("User status changed: ", user);
+    });
 
     return unsubscribe;
   }, []);
@@ -47,12 +64,13 @@ const AuthContext = ({ children }) => {
     loginWithGoogle,
     login,
     logout,
+    resetPassword,
     model,
     setModel,
     alert,
     setAlert,
     loading,
-    setLoading
+    setLoading,
   };
 
   return <authContext.Provider {...{ value }}>{children}</authContext.Provider>;
