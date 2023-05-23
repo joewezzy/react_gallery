001f507c25002e104e83d24f7b27dbecc5d76641
joe sunesis <joemarbell@gmail.com>
2023-05-23 11:12:21 +0000

    Change email and password after re authentication

--- src/components/user/settings/ChangeEmail.js
+++ src/components/user/settings/ChangeEmail.js
@@ -1,17 +1,38 @@
 import { DialogActions, DialogContent, DialogContentText } from '@mui/material'
+import { updateEmail } from 'firebase/auth';
 import React, { useRef } from 'react'
 import { useAuth } from '../../../context/AuthContext';
 import EmailField from '../inputs/EmailField'
 import SubmitButton from '../inputs/SubmitButton';
 
 const ChangeEmail = () => {
-  const {currentUser, setLoading, setAlert, setModel, model0} =useAuth();
+  const {currentUser, setLoading, setAlert, setModel, model} =useAuth();
   const emailRef = useRef();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
 
+    try {
+      await updateEmail(currentUser, emailRef.current.value);
+      setModel({ ...model, isOpen: false });
+      setAlert({
+        isAlert: true,
+        severity: "success",
+        message: "Your email has been updated!",
+        timeout: 8000,
+        location: "main",
+      });
+    } catch (error) {
+      setAlert({
+        isAlert: true,
+        severity: "error",
+        message: error.message,
+        timeout: 5000,
+        location: "main",
+      });
+      console.log(error);      
+    }
     
     setLoading(false);
   };
--- src/components/user/settings/ChangePassword.js
+++ src/components/user/settings/ChangePassword.js
@@ -1,11 +1,12 @@
-import { DialogActions, DialogContent, DialogContentText } from '@mui/material'
-import React, { useRef } from 'react'
-import { useAuth } from '../../../context/AuthContext';
-import PasswordField from '../inputs/PasswordField'
-import SubmitButton from '../inputs/SubmitButton';
+import { DialogActions, DialogContent, DialogContentText } from "@mui/material";
+import { updatePassword } from "firebase/auth";
+import React, { useRef } from "react";
+import { useAuth } from "../../../context/AuthContext";
+import PasswordField from "../inputs/PasswordField";
+import SubmitButton from "../inputs/SubmitButton";
 
 const ChangePassword = () => {
-  const {currentUser, setLoading, setAlert, setModel, model0} =useAuth();
+  const { currentUser, setLoading, setAlert, setModel, model } = useAuth();
   const passwordRef = useRef();
   const confirmPasswordRef = useRef();
 
@@ -13,7 +14,29 @@ const ChangePassword = () => {
     e.preventDefault();
     setLoading(true);
 
-    
+    try {
+      if (passwordRef.current.value !== confirmPasswordRef.current.value)
+        throw new Error("Passwords do not match");
+      await updatePassword(currentUser, passwordRef.current.value);
+      setModel({ ...model, isOpen: false });
+      setAlert({
+        isAlert: true,
+        severity: "success",
+        message: "Your password has been updated!",
+        timeout: 8000,
+        location: "main",
+      });
+    } catch (error) {
+      setAlert({
+        isAlert: true,
+        severity: "error",
+        message: error.message,
+        timeout: 5000,
+        location: "main",
+      });
+      console.log(error);
+    }
+
     setLoading(false);
   };
 
@@ -21,14 +44,20 @@ const ChangePassword = () => {
     <form onSubmit={handleSubmit}>
       <DialogContent>
         <DialogContentText>Please Enter your new Password: </DialogContentText>
-        <PasswordField {...{passwordRef}} />
-        <PasswordField {...{passwordRef: confirmPasswordRef, id: 'confirmPassword', label: 'Confirm Password'}} />
+        <PasswordField {...{ passwordRef }} />
+        <PasswordField
+          {...{
+            passwordRef: confirmPasswordRef,
+            id: "confirmPassword",
+            label: "Confirm Password",
+          }}
+        />
       </DialogContent>
       <DialogActions>
         <SubmitButton />
       </DialogActions>
     </form>
-  )
-}
+  );
+};
 
-export default ChangePassword
+export default ChangePassword;
