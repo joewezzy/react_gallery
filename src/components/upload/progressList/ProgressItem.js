cca370482e4c5d447e1e96ae20985884b9725ee2
joewezzy <joemarbell@gmail.com>
2023-06-28 11:36:28 +0000

    Updated firebase public file for hosting (#16)

--- src/components/upload/progressList/ProgressItem.js
+++ src/components/upload/progressList/ProgressItem.js
@@ -25,7 +25,7 @@ const ProgressItem = ({file}) => {
           uName: currentUser?.displayName || '',
           uPhoto: currentUser?.photoURL || '',
         }
-        await addDocument('user', galleryDoc, imageName);
+        await addDocument('gallery', galleryDoc, imageName);
         setImageUrl(null);
       } catch (error) {
         setAlert({
