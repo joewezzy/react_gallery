83416a96390994528e0530057b9f686d4ce06212
joe sunesis <joemarbell@gmail.com>
2023-06-28 15:59:06 +0000

    Made changes to the deploy script.

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
@@ -11,14 +11,15 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
   projectId: process.env.REACT_APP_projectId,
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = getApps().length === 0 ?  initializeApp(firebaseConfig) : getApp();
 export const storage = getStorage();
 export const db = getFirestore();
 
